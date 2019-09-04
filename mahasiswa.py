ef6931f28a146d55dab60dbbbf3240de1d3554ea
aditya-eko <ekoagusaditya@gmail.com>
2019-09-04 16:36:06 +0700

    menambahkan file mahasiswa.pu

---
+++ mahasiswa.py
@@ -0,0 +1,15 @@
+class Mahasiswa:
+    pass
+
+
+    def __init__(self,inputnama,inputnip,inpputsemester):
+        self.nama=inputnama
+        self.nip=inputnip
+        self.semester=inpputsemester
+
+
+
+eko=Mahasiswa("eko agus aditya",8348949,7)
+
+
+print(eko.nama)
